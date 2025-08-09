ed70f3cdb6887530e4343e47f58d2d091e7bee23
fitsumhelina <dev.fitsum@gmail.com>
2025-08-09 16:01:41 +0300

    Refactor UserSeeder to use firstOrCreate for user creation and remove redundant email field

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,33 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-  
     public function run(): void
     {
-        $admin = \App\Models\User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('12345678'),
-        ]);
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin User',
+                'password' => bcrypt('12345678'),
+            ]
+        );
         $admin->assignRole('admin');
 
-        $moderator = \App\Models\User::create([
-            'name' => 'Moderator User',
-            'email' => 'moderator@moderator.com',
-            'password' => bcrypt('12345678'),
-        ]);
+        $moderator = User::firstOrCreate(
+            ['email' => 'moderator@moderator.com'],
+            [
+                'name' => 'Moderator User',
+                'password' => bcrypt('12345678'),
+            ]
+        );
         $moderator->assignRole('moderator');
 
-        $blogger = \App\Models\User::create([
-            'name' => 'Blogger User',
-            'email' => 'blogger@blogger.com',
-            'password' => bcrypt('12345678'),
-        ]);
+        $blogger = User::firstOrCreate(
+            ['email' => 'blogger@blogger.com'],
+            [
+                'name' => 'Blogger User',
+                'password' => bcrypt('12345678'),
+            ]
+        );
         $blogger->assignRole('blogger');
     }
 }
