208fd718b93303bfd4ce89915c4ec8b03fcb84bb
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-09 17:16:33 +0300

    Refactor EditSocialRef class to improve code structure and ensure updated_by field is set before saving

--- app/Filament/Resources/SocialRefResource/Pages/EditSocialRef.php
+++ app/Filament/Resources/SocialRefResource/Pages/EditSocialRef.php
@@ -5,15 +5,18 @@ namespace App\Filament\Resources\SocialRefResource\Pages;
 use App\Filament\Resources\SocialRefResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
-class EditSocialRef extends EditRecord
-{
+class EditSocialRef extends EditRecord {
     protected static string $resource = SocialRefResource::class;
 
-    protected function getHeaderActions(): array
-    {
+    protected function getHeaderActions(): array {
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+    protected function mutateFormDataBeforeSave( array $data ): array {
+        $data[ 'updated_by' ] = Auth::user()?->id;
+        return $data;
     }
 }
