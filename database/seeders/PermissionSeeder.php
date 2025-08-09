57bba9db4045081d5ce16ef1fccdde5f356389ad
fitsumhelina <dev.fitsum@gmail.com>
2025-08-09 15:37:28 +0300

    Add Spatie roles trait to User model and implement permission seeder for roles and permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PermissionSeeder extends Seeder
 {
@@ -12,6 +13,30 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $entities = ['blog', 'service', 'team'];
+        $actions = ['create', 'read', 'update', 'delete'];
+        $roles = ['admin', 'blogger', 'moderator'];
+
+        // Insert permissions
+        foreach ($entities as $entity) {
+            foreach ($actions as $action) {
+            DB::table('permissions')->insert([
+                'name' => "{$action}_{$entity}",
+                'guard_name' => 'web',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            }
+        }
+
+        // Insert roles
+        foreach ($roles as $role) {
+            DB::table('roles')->insert([
+            'name' => $role,
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]);
+        }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,11 +7,28 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+  
     public function run(): void
     {
-        //
+        $admin = \App\Models\User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $admin->assignRole('admin');
+
+        $moderator = \App\Models\User::create([
+            'name' => 'Moderator User',
+            'email' => 'moderator@moderator.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $moderator->assignRole('moderator');
+
+        $blogger = \App\Models\User::create([
+            'name' => 'Blogger User',
+            'email' => 'blogger@blogger.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $blogger->assignRole('blogger');
     }
 }
