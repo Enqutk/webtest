f4425516d32b06f215e8db06ec372367ec08bdf9
fitsumhelina <dev.fitsum@gmail.com>
2025-08-09 15:39:39 +0300

    Add GenericPolicy class to handle authorization for create, update, and delete actions

---
+++ app/Policies/GenericPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Str;
+
+class GenericPolicy
+{
+    use HandlesAuthorization;
+
+    public function create(User $user, $model)
+    {
+        return $user->hasPermissionTo('create ' . Str::kebab(class_basename($model)));
+    }
+
+    public function update(User $user, $model)
+    {
+        return $user->hasPermissionTo('update ' . Str::kebab(class_basename($model)));
+    }
+
+    public function delete(User $user, $model)
+    {
+        return $user->hasPermissionTo('delete ' . Str::kebab(class_basename($model)));
+    }
+}
