9a09b5b4d267be8cf1c63eb4f3fdd7ec42c5ef1c
fitsumhelina <dev.fitsum@gmail.com>
2025-08-11 10:22:39 +0300

    feat: add Organization model and migration for organizations table with necessary fields

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organization extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'po_box',
+        'address',
+        'opening_hours',
+        'map_url',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => 'string',
+    ];
+
+    public function contacts()
+    {
+        return $this->hasMany(OrganizationContact::class);
+    }
 }
