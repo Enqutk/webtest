1c6ae3d50e5082d9b341e2c31bd70b1df321300b
fitsumhelina <dev.fitsum@gmail.com>
2025-08-11 10:35:33 +0300

    refactor: remove unnecessary docblock comment from OrganizationSeeder

--- database/seeders/OrganizationSeeder.php
+++ database/seeders/OrganizationSeeder.php
@@ -8,9 +8,7 @@ use Illuminate\Database\Seeder;
 
 class OrganizationSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+  
     public function run(): void
     {
         $organization = Organization::create([
