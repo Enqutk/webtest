66274a4dea8ae715b8438cf14e28b86403e28132
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-11 16:41:15 +0300

    feat: create migration for entities table with necessary fields and relationships

---
+++ database/migrations/2025_08_11_133854_create_entities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\StatusEnum;
+use App\Enums\EntityTypeEnum;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type')->default(EntityTypeEnum::Client->value);
+            $table->string('link')->nullable();
+            $table->string('image_path')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('order')->default(0);
+            $table->string('status')->default(StatusEnum::active->value);
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('entities');
+    }
+};
