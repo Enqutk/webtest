8fcf92ea80070cd1db8f5ce113f052101eca25b5
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-11 09:54:30 +0300

    Refactor ListSocialRefs for improved code formatting and consistency; streamline method definitions for better readability.

--- app/Filament/Resources/SocialRefResource/Pages/ListSocialRefs.php
+++ app/Filament/Resources/SocialRefResource/Pages/ListSocialRefs.php
@@ -6,10 +6,12 @@ use App\Filament\Resources\SocialRefResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
-class ListSocialRefs extends ListRecords {
+class ListSocialRefs extends ListRecords
+{
     protected static string $resource = SocialRefResource::class;
 
-    protected function getHeaderActions(): array {
+    protected function getHeaderActions(): array
+    {
         return [
             Actions\CreateAction::make(),
         ];
