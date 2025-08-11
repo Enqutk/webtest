706a15100a4ed28d5bae800c01b725ea5148e808
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-11 14:06:59 +0300

    refactor: ensure consistent formatting in SetsCreatedAndUpdatedBy trait methods

--- app/Filament/Traits/SetsCreatedAndUpdatedBy.php
+++ app/Filament/Traits/SetsCreatedAndUpdatedBy.php
@@ -6,10 +6,16 @@ use Illuminate\Support\Facades\Auth;
 
 trait SetsCreatedAndUpdatedBy
 {
-  protected function mutateFormDataBeforeCreate(array $data): array
-  {
-    $data['created_by'] = Auth::id();
-    $data['updated_by'] = Auth::id();
-    return $data;
-  }
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_by'] = Auth::id();
+        $data['updated_by'] = Auth::id();
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['updated_by'] = Auth::id();
+        return $data;
+    }
 }
