5e3ae9d30047daa638a4fbdbc1823bfbde1afa2c
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-11 14:55:12 +0300

    refactor: remove SetsCreatedAndUpdatedBy trait from CreateHero class

--- app/Filament/Resources/HeroResource/Pages/CreateHero.php
+++ app/Filament/Resources/HeroResource/Pages/CreateHero.php
@@ -4,11 +4,11 @@ namespace App\Filament\Resources\HeroResource\Pages;
 
 use App\Filament\Resources\HeroResource;
 use Filament\Resources\Pages\CreateRecord;
-use App\Filament\Traits\SetsCreatedAndUpdatedBy;
+
 
 class CreateHero extends CreateRecord
 {
-    use SetsCreatedAndUpdatedBy;
+  
 
     protected static string $resource = HeroResource::class;
 }
