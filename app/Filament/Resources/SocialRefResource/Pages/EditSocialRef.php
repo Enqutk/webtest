0373a41eb83964a54a0d7a830a4861a0aac6135b
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-11 14:55:31 +0300

    refactor: remove SetsCreatedAndUpdatedBy trait from EditSocialRef class

--- app/Filament/Resources/SocialRefResource/Pages/EditSocialRef.php
+++ app/Filament/Resources/SocialRefResource/Pages/EditSocialRef.php
@@ -5,11 +5,11 @@ namespace App\Filament\Resources\SocialRefResource\Pages;
 use App\Filament\Resources\SocialRefResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-use App\Filament\Traits\SetsCreatedAndUpdatedBy;
+
 
 class EditSocialRef extends EditRecord
 {
-    use SetsCreatedAndUpdatedBy;
+
     protected static string $resource = SocialRefResource::class;
 
     protected function getHeaderActions(): array
