5b4bd042c7bcd0ad19c23f87475dd48435276b2d
fitsumhelina <dev.fitsum@gmail.com>
2025-08-11 10:31:49 +0300

    feat: update DatabaseSeeder to include OrganizationSeeder and refactor OrganizationSeeder for cleaner code

--- database/seeders/OrganizationSeeder.php
+++ database/seeders/OrganizationSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Organization;
+use App\Models\OrganizationContact;
 use Illuminate\Database\Seeder;
 
 class OrganizationSeeder extends Seeder
@@ -11,7 +13,7 @@ class OrganizationSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Organization::create([
+        $organization = Organization::create([
             'title' => 'Demo Organization',
             'po_box' => 'PO Box 12345',
             'address' => '123 Demo Street, Demo City, Country',
@@ -20,6 +22,24 @@ class OrganizationSeeder extends Seeder
             'status' => 'active',
             'created_at' => now(),
             'updated_at' => now(),
+        ]);
+
+        OrganizationContact::create([
+            'organization_id' => $organization->id,
+            'type' => 'phone',
+            'value' => '+1234567890',
+        ]);
+
+        OrganizationContact::create([
+            'organization_id' => $organization->id,
+            'type' => 'fax',
+            'value' => '+1234567891',
+        ]);
+
+        OrganizationContact::create([
+            'organization_id' => $organization->id,
+            'type' => 'email',
+            'value' => 'info@demo.org',
         ]);
     }
 }
