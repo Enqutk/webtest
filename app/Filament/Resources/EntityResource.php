18d149490b2c391f1b22213421174d96d83a890e
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-11 17:48:37 +0300

    refactor: improve formatting and structure of EntityResource form and table methods

--- app/Filament/Resources/EntityResource.php
+++ app/Filament/Resources/EntityResource.php
@@ -16,62 +16,67 @@ use App\Enums\StatusEnum;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class EntityResource extends Resource {
+class EntityResource extends Resource
+{
     protected static ?string $model = Entity::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form( Form $form ): Form {
+    public static function form(Form $form): Form
+    {
         return $form
-        ->schema( [
-            Forms\Components\TextInput::make( 'name' )
-            ->required()
-            ->maxLength( 255 ),
-            Forms\Components\Select::make( 'type' )
-            ->options([
-                EntityTypeEnum::client->value => 'Client',
-                EntityTypeEnum::partner->value => 'Partner',
-                EntityTypeEnum::award->value => 'Award',
-            ])
-            ->required(),
-            Forms\Components\TextInput::make( 'link' )
-            ->maxLength( 255 )
-            ->url()
-            ->nullable(),
-            Forms\Components\FileUpload::make( 'image_path' )
-            ->directory( 'entities' )
-            ->disk( 'public' )
-            ->image()
-            ->imagePreviewHeight( '150' )
-            ->nullable(),
-            Forms\Components\Textarea::make( 'description' )
-            ->maxLength( 65535 )
-            ->nullable(),
-            Forms\Components\TextInput::make('order')
-                ->numeric()
-                ->default(1)
-                ->minValue(1)
-                ->unique(),
-            Forms\Components\Select::make( 'status' )
-            ->options( StatusEnum::class )
-            ->default( 'active' )
-            ->required(),
-        ] );
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('type')
+                    ->options([
+                        EntityTypeEnum::client->value => 'Client',
+                        EntityTypeEnum::partner->value => 'Partner',
+                        EntityTypeEnum::award->value => 'Award',
+                    ])
+                    ->required(),
+                Forms\Components\TextInput::make('link')
+                    ->maxLength(255)
+                    ->url()
+                    ->nullable(),
+                Forms\Components\FileUpload::make('image_path')
+                    ->directory('entities')
+                    ->disk('public')
+                    ->image()
+                    ->imagePreviewHeight('150')
+                    ->nullable()
+                    ->required(),
+                Forms\Components\Textarea::make('description')
+                    ->maxLength(65535)
+                    ->nullable(),
+                Forms\Components\TextInput::make('order')
+                    ->numeric()
+                    ->default(1)
+                    ->minValue(1)
+                    ->unique()
+                    ->required(),
+                Forms\Components\Select::make('status')
+                    ->options(StatusEnum::class)
+                    ->default('active')
+                    ->required(),
+            ]);
     }
 
-    public static function table( Table $table ): Table {
+    public static function table(Table $table): Table
+    {
         return $table
-        ->columns( [
-            Tables\Columns\TextColumn::make( 'id' )->sortable(),
-            Tables\Columns\TextColumn::make( 'name' )->searchable()->sortable(),
-            Tables\Columns\TextColumn::make( 'type' )->badge()->sortable(),
-            Tables\Columns\ImageColumn::make( 'image_path' )
-            ->disk( 'public' )
-            ->square()
-            ->size( 50 )
-            ->url( fn ( $record ) => $record->image_path ? asset( 'storage/entities/' . basename( $record->image_path ) ) : null ),
-            Tables\Columns\TextColumn::make( 'link' )->url( fn ( $record ) => $record->link )->openUrlInNewTab( true )->searchable(),
-            Tables\Columns\TextColumn::make( 'order' )->sortable(),
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->sortable(),
+                Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('type')->badge()->sortable(),
+                Tables\Columns\ImageColumn::make('image_path')
+                    ->disk('public')
+                    ->square()
+                    ->size(50)
+                    ->url(fn($record) => $record->image_path ? asset('storage/entities/' . basename($record->image_path)) : null),
+                Tables\Columns\TextColumn::make('link')->url(fn($record) => $record->link)->openUrlInNewTab(true)->searchable(),
+                Tables\Columns\TextColumn::make('order')->sortable(),
                 Tables\Columns\TextColumn::make('status')
                     ->badge()
                     ->color(fn(StatusEnum $state): string => match ($state) {
@@ -79,24 +84,24 @@ class EntityResource extends Resource {
                         StatusEnum::inactive => 'danger',
                     })
                     ->sortable(),
-            Tables\Columns\TextColumn::make( 'created_at' )->dateTime()->sortable()->toggleable( isToggledHiddenByDefault: true ),
-            Tables\Columns\TextColumn::make( 'updated_at' )->dateTime()->sortable()->toggleable( isToggledHiddenByDefault: true ),
-            Tables\Columns\TextColumn::make( 'deleted_at' )->dateTime()->sortable()->toggleable( isToggledHiddenByDefault: true ),
-        ] )
-        ->filters( [
-            Tables\Filters\TrashedFilter::make(),
-            Tables\Filters\SelectFilter::make('type')
-                ->options([
-                    EntityTypeEnum::client->value => 'Client',
-                    EntityTypeEnum::partner->value => 'Partner',
-                    EntityTypeEnum::award->value => 'Award',
-                ]),
-            Tables\Filters\SelectFilter::make('status')
-                ->options([
-                    StatusEnum::active->value => 'Active',
-                    StatusEnum::inactive->value => 'Inactive',
-                ]),
-        ] )
+                Tables\Columns\TextColumn::make('created_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+                Tables\Filters\SelectFilter::make('type')
+                    ->options([
+                        EntityTypeEnum::client->value => 'Client',
+                        EntityTypeEnum::partner->value => 'Partner',
+                        EntityTypeEnum::award->value => 'Award',
+                    ]),
+                Tables\Filters\SelectFilter::make('status')
+                    ->options([
+                        StatusEnum::active->value => 'Active',
+                        StatusEnum::inactive->value => 'Inactive',
+                    ]),
+            ])
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
@@ -113,17 +118,19 @@ class EntityResource extends Resource {
             ->defaultSort('order');
     }
 
-    public static function getRelations(): array {
+    public static function getRelations(): array
+    {
         return [
             //
         ];
     }
 
-    public static function getPages(): array {
+    public static function getPages(): array
+    {
         return [
-            'index' => Pages\ListEntities::route( '/' ),
-            'create' => Pages\CreateEntity::route( '/create' ),
-            'edit' => Pages\EditEntity::route( '/{record}/edit' ),
+            'index' => Pages\ListEntities::route('/'),
+            'create' => Pages\CreateEntity::route('/create'),
+            'edit' => Pages\EditEntity::route('/{record}/edit'),
         ];
     }
 }
