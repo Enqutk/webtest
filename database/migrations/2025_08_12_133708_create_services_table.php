805be63ece7d82ae58a88740b7adc811b79a0cae
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-12 17:07:39 +0300

    feat: create migration for services table with necessary fields and relationships

---
+++ database/migrations/2025_08_12_133708_create_services_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\StatusEnum;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->longText('title');
+            $table->longText('svg_path')->nullable();
+            $table->longText('image_1_path')->nullable();
+            $table->longText('image_2_path')->nullable();
+            $table->string('short_description', 2000)->nullable();
+            $table->longText('quote')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('features')->nullable();
+            $table->unsignedInteger('order')->default(1);
+            $table->string('status')->default(StatusEnum::active->value);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->index('status');
+            $table->index('order');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
