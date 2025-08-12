d1bd26abdd65b33b53f65cd6cd9a9a1ef417c4ad
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-12 14:13:38 +0300

    refactor: adjust formatting of relationship methods in Entity model for consistency

--- app/Models/Entity.php
+++ app/Models/Entity.php
@@ -34,12 +34,14 @@ class Entity extends Model implements HasMedia
 
     // Relationships
 
-    public function creator():BelongsTo {
-        return $this->belongsTo( User::class, 'created_by' );
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 
-    public function updater(): BelongsTo {
-        return $this->belongsTo( User::class, 'updated_by' );
+    public function updater(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
     }
 
     public function registerMediaCollections(): void {
