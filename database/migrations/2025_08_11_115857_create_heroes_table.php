749b89a3c306aec2b501609238c5098321e7a67a
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-12 14:41:25 +0300

    refactor: remove unused index on 'type' column in heroes table migration

--- database/migrations/2025_08_11_115857_create_heroes_table.php
+++ database/migrations/2025_08_11_115857_create_heroes_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
             $table->softDeletes();
             $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
             $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
-            $table->index('type');
             $table->index('status');
             $table->index('order');
         });
