e5c36e4ba272d84a9fbe78131a60779ae64cf1d4
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-13 08:48:40 +0300

    feat: update ServiceResource form and table to include new image fields and improve media handling

--- app/Filament/Resources/ServiceResource.php
+++ app/Filament/Resources/ServiceResource.php
@@ -16,126 +16,121 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 
-class ServiceResource extends Resource
-{
+class ServiceResource extends Resource {
     protected static ?string $model = Service::class;
     protected static ?string $navigationGroup = 'Other';
     protected static ?int $navigationSort = 2;
     protected static ?string $navigationLabel = 'services';
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
+    public static function form( Form $form ): Form {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('slug')
-                    ->required()
-                    ->unique(),
-                Forms\Components\TextInput::make('title')
-                    ->required(),
-                Forms\Components\SpatieMediaLibraryFileUpload::make('svg_path')
-                    ->image()
-                    ->acceptedFileTypes(['image/svg+xml'])
-                    ->imagePreviewHeight('150'),
-                Forms\Components\SpatieMediaLibraryFileUpload::make('image 1')
-                    ->image()
-                    ->imagePreviewHeight('150'),
-                Forms\Components\SpatieMediaLibraryFileUpload::make('image 2')
-                    ->image()
-                    ->imagePreviewHeight('150'),
-                Forms\Components\Textarea::make('short_description')
-                    ->maxLength(65535)
-                    ->nullable(),
-                Forms\Components\Textarea::make('quote')
-                    ->rows(2)
-                    ->maxLength(65535)
-                    ->nullable(),
-                Forms\Components\Textarea::make('description')
-                    ->maxLength(65535)
-                    ->nullable(),
-                Forms\Components\Textarea::make('features')
-                    ->maxLength(65535)
-                    ->nullable(),
-                Forms\Components\TextInput::make('order')
-                    ->numeric()
-                    ->default(1)
-                    ->minValue(1)
-                    ->unique(ignoreRecord: true)
-                    ->required(),
-                Forms\Components\Select::make('status')
-                    ->options(StatusEnum::class)
-                    ->default(StatusEnum::active),
-            ]);
+        ->schema( [
+            Forms\Components\TextInput::make( 'slug' )
+            ->required()
+            ->unique( ignoreRecord: true ),
+            Forms\Components\TextInput::make( 'title' )
+            ->required(),
+            Forms\Components\SpatieMediaLibraryFileUpload::make( 'svg_path' )
+            ->collection( 'svg_path' )
+            ->image()
+            ->acceptedFileTypes( [ 'image/svg+xml' ] )
+            ->imagePreviewHeight( 150 ),
+            Forms\Components\SpatieMediaLibraryFileUpload::make( 'image_1_path' )
+            ->collection( 'image_1_path' )
+            ->image()
+            ->imagePreviewHeight( 150 ),
+            Forms\Components\SpatieMediaLibraryFileUpload::make( 'image_2_path' )
+            ->collection( 'image_2_path' )
+            ->image()
+            ->imagePreviewHeight( 150 ),
+            Forms\Components\Textarea::make( 'short_description' )
+            ->maxLength( 65535 )
+            ->nullable(),
+            Forms\Components\Textarea::make( 'quote' )
+            ->rows( 2 )
+            ->maxLength( 65535 )
+            ->nullable(),
+            Forms\Components\Textarea::make( 'description' )
+            ->maxLength( 65535 )
+            ->nullable(),
+            Forms\Components\Textarea::make( 'features' )
+            ->maxLength( 65535 )
+            ->nullable(),
+            Forms\Components\TextInput::make( 'order' )
+            ->numeric()
+            ->default( 1 )
+            ->minValue( 1 )
+            ->unique( ignoreRecord: true )
+            ->required(),
+            Forms\Components\Select::make( 'status' )
+            ->options( StatusEnum::class )
+            ->default( StatusEnum::active ),
+        ] );
     }
 
-    public static function table(Table $table): Table
-    {
+    public static function table( Table $table ): Table {
         return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('id')->sortable(),
-                Tables\Columns\TextColumn::make('slug')->searchable()->sortable(),
-                Tables\Columns\TextColumn::make('title')->searchable(),
-                Tables\Columns\SpatieMediaLibraryImageColumn::make('image')
-                    ->square()
-                    ->size(50)
-                    ->url(fn($record) => $record->getFirstMediaUrl('image', 'thumb') ?: null),
-                Tables\Columns\SpatieMediaLibraryImageColumn::make('image')
-                    ->square()
-                    ->size(50)
-                    ->url(fn($record) => $record->getFirstMediaUrl('image', 'thumb') ?: null),
-                Tables\Columns\SpatieMediaLibraryImageColumn::make('image')
-                    ->square()
-                    ->size(50)
-                    ->url(fn($record) => $record->getFirstMediaUrl('image', 'thumb') ?: null),
-
-                Tables\Columns\TextColumn::make('short_description')->limit(50),
-                Tables\Columns\TextColumn::make('order')->sortable(),
-                Tables\Columns\TextColumn::make('status')->badge(),
-                Tables\Columns\TextColumn::make('created_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('creator.name')->label('Created By')->sortable()->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updater.name')->label('Updated By')->sortable()->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('deleted_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
-
-            ])
-            ->filters([
-                Tables\Filters\SelectFilter::make('status')
-                    ->options(\App\Enums\StatusEnum::class),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
-                Tables\Actions\ForceDeleteAction::make(),
-                Tables\Actions\RestoreAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ])
-            ->defaultSort('order');
+        ->columns( [
+            Tables\Columns\TextColumn::make( 'id' )->sortable(),
+            Tables\Columns\TextColumn::make( 'slug' )->searchable()->sortable(),
+            Tables\Columns\TextColumn::make( 'title' )->searchable(),
+            Tables\Columns\SpatieMediaLibraryImageColumn::make( 'svg_path' )
+            ->collection( 'svg_path' )
+            ->label( 'SVG' )
+            ->size( 50 ),
+            Tables\Columns\SpatieMediaLibraryImageColumn::make( 'image_1_path' )
+            ->collection( 'image_1_path' )
+            ->label( 'Image 1' )
+            ->size( 50 ),
+            Tables\Columns\SpatieMediaLibraryImageColumn::make( 'image_2_path' )
+            ->collection( 'image_2_path' )
+            ->label( 'Image 2' )
+            ->size( 50 ),
+            Tables\Columns\TextColumn::make( 'short_description' )->limit( 50 ),
+            Tables\Columns\TextColumn::make( 'order' )->sortable(),
+            Tables\Columns\TextColumn::make( 'status' )->badge(),
+            Tables\Columns\TextColumn::make( 'created_at' )->dateTime()->sortable()->toggleable( isToggledHiddenByDefault: true ),
+            Tables\Columns\TextColumn::make( 'updated_at' )->dateTime()->sortable()->toggleable( isToggledHiddenByDefault: true ),
+            Tables\Columns\TextColumn::make( 'creator.name' )->label( 'Created By' )->sortable()->toggleable( isToggledHiddenByDefault: true ),
+            Tables\Columns\TextColumn::make( 'updater.name' )->label( 'Updated By' )->sortable()->toggleable( isToggledHiddenByDefault: true ),
+            Tables\Columns\TextColumn::make( 'deleted_at' )->dateTime()->sortable()->toggleable( isToggledHiddenByDefault: true ),
+        ] )
+        ->filters( [
+            Tables\Filters\SelectFilter::make( 'status' )
+            ->options( \App\Enums\StatusEnum::class ),
+        ] )
+        ->actions( [
+            Tables\Actions\EditAction::make(),
+            Tables\Actions\DeleteAction::make(),
+            Tables\Actions\ForceDeleteAction::make(),
+            Tables\Actions\RestoreAction::make(),
+        ] )
+        ->bulkActions( [
+            Tables\Actions\BulkActionGroup::make( [
+                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
+            ] ),
+        ] )
+        ->defaultSort( 'order' );
     }
 
-    public static function getRelations(): array
-    {
+    public static function getRelations(): array {
         return [
             //
         ];
     }
-    public static function getEloquentQuery(): Builder
-    {
-        return parent::getEloquentQuery()->withoutGlobalScopes([
+    public static function getEloquentQuery(): Builder {
+        return parent::getEloquentQuery()->withoutGlobalScopes( [
             SoftDeletingScope::class,
-        ]);
+        ] );
     }
-    public static function getPages(): array
-    {
+    public static function getPages(): array {
         return [
-            'index' => Pages\ListServices::route('/'),
-            'create' => Pages\CreateService::route('/create'),
-            'edit' => Pages\EditService::route('/{record}/edit'),
+            'index' => Pages\ListServices::route( '/' ),
+            'create' => Pages\CreateService::route( '/create' ),
+            'edit' => Pages\EditService::route( '/{record}/edit' ),
         ];
     }
 }
