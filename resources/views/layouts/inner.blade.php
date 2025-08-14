0f0a07c757c589fda86a90dd7ec598404d3bb80c
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-14 15:03:52 +0300

    feat: update asset paths to use asset helper for improved asset management

--- resources/views/layouts/inner.blade.php
+++ resources/views/layouts/inner.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html class="no-js" lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -12,30 +13,31 @@
     <!-- CSS
         ============================================ -->
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <!-- Fontawesome -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
     <!-- Pbmit Induyst Icon -->
-    <link rel="stylesheet" href="assets/fonts/pbmit-induyst-icon/pbmit_induyst.css">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/pbmit-induyst-icon/pbmit_induyst.css') }}">
     <!-- Base Icons -->
-    <link rel="stylesheet" href="assets/css/pbminfotech-base-icons.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/pbminfotech-base-icons.css') }}">
     <!-- Themify Icons -->
-    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}">
     <!-- Slick -->
-    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper.min.css') }}">
     <!-- Magnific -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
     <!-- AOS -->
-    <link rel="stylesheet" href="assets/css/aos.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/aos.css') }}">
     <!-- Shortcode CSS -->
-    <link rel="stylesheet" href="assets/css/shortcode.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/shortcode.css') }}">
     <!-- Base CSS -->
-    <link rel="stylesheet" href="assets/css/base.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/base.css') }}">
     <!-- Style CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- Responsive CSS -->
-    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
 </head>
+
 <body>
     <!-- Page Wrapper -->
     <div class="page-wrapper" id="page">
@@ -57,18 +59,20 @@
                         <div class="pbmit-breadcrumb">
                             <div class="pbmit-breadcrumb-inner">
                                 <span>
-                                    <a title="" href="{{ route('home') }}" class="home"><span>Veritas Afrika</span></a>
+                                    <a title="" href="{{ route('home') }}" class="home"><span>Veritas
+                                            Afrika</span></a>
                                 </span>
                                 <span class="sep"></span>
-                                <span><span class="post-root post post-post current-item">@yield('page_title')</span></span>
+                                <span><span
+                                        class="post-root post post-post current-item">@yield('page_title')</span></span>
                             </div>
                         </div>
                     </div>
-                </div> 
-            </div> 
+                </div>
+            </div>
         </div>
         <!-- Title Bar End-->
-         
+
         <!-- Page Content -->
         <div class="page-content">
             @yield('content')
@@ -84,13 +88,15 @@
             <div class="pbmit-search-overlay"></div>
             <div class="pbmit-header-search-form-wrapper">
                 <div class="pbmit-search-close">
-                    <svg class="qodef-svg--close qodef-m" xmlns="http://www.w3.org/2000/svg" width="28.163" height="28.163" viewBox="0 0 26.163 26.163">
+                    <svg class="qodef-svg--close qodef-m" xmlns="http://www.w3.org/2000/svg" width="28.163"
+                        height="28.163" viewBox="0 0 26.163 26.163">
                         <rect width="36" height="1" transform="translate(0.707) rotate(45)"></rect>
                         <rect width="36" height="1" transform="translate(0 25.456) rotate(-45)"></rect>
                     </svg>
                 </div>
                 <form role="search" method="get" class="search-form" action="#">
-                    <input type="search" id="search-form" class="search-field" placeholder="Search …" value="" name="s">
+                    <input type="search" id="search-form" class="search-field" placeholder="Search …" value=""
+                        name="s">
                     <button type="submit" class="search-submit " title="Search"></button>
                     <div class="pbmit-search-line"></div>
                 </form>
@@ -107,48 +113,49 @@
                 <i class="pbmit-base-icon-up-open-big"></i>
             </div>
         </div>
-        <!-- Scroll To Top End -->
-         
+        <!-- Scroll To Top
+
     </div>
     <!-- Page Wrapper End -->
 
-    <!-- JS 
+        <!-- JS
         ============================================ -->
-    <!-- jQuery JS -->
-    <script src="assets/js/jquery.min.js"></script>
-    <!-- Popper JS -->
-    <script src="assets/js/popper.min.js"></script>
-    <!-- Bootstrap JS -->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!-- jquery Waypoints JS -->
-    <script src="assets/js/jquery.waypoints.min.js"></script>
-    <!-- jquery Appear JS -->
-    <script src="assets/js/jquery.appear.js"></script>
-    <!-- Numinate JS -->
-    <script src="assets/js/numinate.min.js"></script>
-    <!-- Slick JS -->
-    <script src="assets/js/swiper.min.js"></script>
-    <!-- Magnific JS -->
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <!-- Circle Progress JS -->
-    <script src="assets/js/circle-progress.js"></script> 
-    <!-- countdown JS -->
-    <script src="assets/js/jquery.countdown.min.js"></script> 
-    <!-- AOS -->
-    <script src="assets/js/aos.js"></script>
-    <!-- GSAP -->
-    <script src='assets/js/gsap.js'></script>
-    <!-- Scroll Trigger -->
-    <script src='assets/js/ScrollTrigger.js'></script>
-    <!-- Split Text -->
-    <script src='assets/js/SplitText.js'></script>
-    <!-- Theia Sticky Sidebar JS -->
-    <script src='assets/js/theia-sticky-sidebar.js'></script>
-    <!-- GSAP Animation -->
-    <script src='assets/js/gsap-animation.js'></script>
-    <!-- Scripts JS -->
-    <script src="assets/js/scripts.js"></script>
+        <!-- jQuery JS -->
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <!-- Popper JS -->
+        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+        <!-- Bootstrap JS -->
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+        <!-- jquery Waypoints JS -->
+        <script src="{{ asset('assets/js/jquery.waypoints.min.js') }}"></script>
+        <!-- jquery Appear JS -->
+        <script src="{{ asset('assets/js/jquery.appear.js') }}"></script>
+        <!-- Numinate JS -->
+        <script src="{{ asset('assets/js/numinate.min.js') }}"></script>
+        <!-- Slick JS -->
+        <script src="{{ asset('assets/js/swiper.min.js') }}"></script>
+        <!-- Magnific JS -->
+        <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+        <!-- Circle Progress JS -->
+        <script src="{{ asset('assets/js/circle-progress.js') }}"></script>
+        <!-- countdown JS -->
+        <script src="{{ asset('assets/js/jquery.countdown.min.js') }}"></script>
+        <!-- AOS -->
+        <script src="{{ asset('assets/js/aos.js') }}"></script>
+        <!-- GSAP -->
+        <script src="{{ asset('assets/js/gsap.js') }}"></script>
+        <!-- Scroll Trigger -->
+        <script src="{{ asset('assets/js/ScrollTrigger.js') }}"></script>
+        <!-- Split Text -->
+        <script src="{{ asset('assets/js/SplitText.js') }}"></script>
+        <!-- Theia Sticky Sidebar JS -->
+        <script src="{{ asset('assets/js/theia-sticky-sidebar.js') }}"></script>
+        <!-- GSAP Animation -->
+        <script src="{{ asset('assets/js/gsap-animation.js') }}"></script>
+        <!-- Scripts JS -->
+        <script src="{{ asset('assets/js/scripts.js') }}"></script>
 
-    @stack('scripts')
+        @stack('scripts')
 </body>
+
 </html>
