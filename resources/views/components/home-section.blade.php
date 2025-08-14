513fc039cb1a1ca6610d749cdd6c84582a35c729
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-14 10:23:09 +0300

    refactor: simplify image URL retrieval in HomeSection component

--- resources/views/components/home-section.blade.php
+++ resources/views/components/home-section.blade.php
@@ -1,16 +1,11 @@
-
 <div class="pbmit-slider-area pbmit-slider-two">
     <div class="swiper-slider" data-autoplay="true" data-loop="true" data-dots="false" data-arrows="true" data-columns="1"
         data-margin="0" data-effect="fade">
         <div class="swiper-wrapper">
             @foreach ($heroes as $hero)
-                @php
-                    $imageUrl = $hero->getFirstMediaUrl('image');
-                @endphp
-
                 <div class="swiper-slide" id="hero-slide-{{ $hero->id }}">
                     <div class="pbmit-slider-item">
-                        <div class="pbmit-slider-bg" style="background-image: url('{{ $imageUrl }}');"></div>
+                        <div class="pbmit-slider-bg" style="background-image: url('{{ $hero->image_url }}');"></div>
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-8 col-1">
@@ -22,7 +17,6 @@
                                             <span class="first">{{ $hero->title }}</span>
                                         </h2>
                                         <p class="pbmit-slider-desc"><span>{{ $hero->description }}</span></p>
-
                                         <div class="pbmit-button d-flex align-items-center">
                                             <div class="transform-bottom transform-delay-4">
                                                 <a href="{{ $hero->button_link ?? '#' }}" class="pbmit-btn white">
@@ -36,7 +30,8 @@
                                             </div>
                                             <div class="ms-4 transform-delay-5">
                                                 <div class="second-btn">
-                                                    <a class="pbmit-btn-style-text white" href="{{ $hero->text_link ?? '#' }}">
+                                                    <a class="pbmit-btn-style-text white"
+                                                        href="{{ $hero->text_link ?? '#' }}">
                                                         <span class="pbmit-button-text">Explore Our Services</span>
                                                     </a>
                                                 </div>
