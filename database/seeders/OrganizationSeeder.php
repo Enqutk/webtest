b46d8581d2958895d580423796fe45e13fe2ad14
fitsumhelina <dev.fitsum@gmail.com>
2025-08-14 15:00:25 +0300

    fix: increase map_url length in organizations table and update seed data

--- database/seeders/OrganizationSeeder.php
+++ database/seeders/OrganizationSeeder.php
@@ -22,7 +22,7 @@ class OrganizationSeeder extends Seeder
                     'to' => '17:00:00',
                 ],
             ],
-            'map_url' => 'https://maps.example.com/demo-org',
+            'map_url' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4447.059743584574!2d55.236811157874726!3d25.15773988483846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69680772d039%3A0x13085a14a32378e7!2sDesert%20Dunes%20building%20contracting%20llc!5e0!3m2!1sen!2set!4v1755167555176!5m2!1sen!2set',
             'status' => 'active',
         ]);
 
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -332,3 +332,4 @@
 </section>
 <!-- Map Section End -->
 @endsection
+
