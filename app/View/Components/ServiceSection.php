4b269f44f299ab165453c5314b15d9afbd0d093f
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-14 09:57:24 +0300

    feat: add ServiceSection component to manage and display active services

---
+++ app/View/Components/ServiceSection.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use App\Models\Service;
+use App\Enums\StatusEnum;
+
+class ServiceSection extends Component
+{   public $services;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct() {
+        $this->services = Service::where( 'status', StatusEnum::active )->orderBy( 'order' )->get();
+    }
+
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.service-section');
+    }
+}
+
+
+
