d5d4738a5546a36ac28c1b464cb36d8f2722cedd
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-14 15:03:34 +0300

    feat: update footer layout and improve logo asset handling

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,126 +1,128 @@
 <!-- footer -->
 <footer class="site-footer pbmit-footer-style-1 pbmit-bg-color-blackish">
-	<div class="pbmit-footer-big-area-wrapper">
-		<div class="pbmit-footer-big-area">
-			<div class="container">
-				<div class="row align-items-center">
-					<div class="col-md-12 col-lg-6 pbmit-footer-left">
-						<h3>Delivering excellence, one project at a time.</h3>
-					</div>
-					<div class="col-md-12 col-lg-6 text-lg-end">
-						<div class="pbmit-footer-logo">
-							<img class="pbmit-main-logo" src="./assets/images/logo/logo-05.png" alt="">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="pbmit-footer-contact-area-wrapper">
-		<div class="pbmit-footer-contact-area">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12 col-lg-4 pbmit-footer-contact-box">
-						<span>Hia cinema, JubaSouth Sudan</span>
-					</div>
-					<div class="col-md-12 col-lg-4 pbmit-footer-contact-box">
-						<span><a href="https://induyst-demo.pbminfotech.com/cdn-cgi/l/email-protection"
-								class="__cf_email__"
-								data-cfemail="d6b5b9b8a2b7b5a296b3aeb7bba6bab3f8b5b9bb">[email&#160;protected]</a></span>
-					</div>
-					<div class="col-md-12 col-lg-4 pbmit-footer-contact-box">
-						<span>+211 923 2 41 605</span>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="pbmit-footer-widget-area">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-lg-3 pbmit-footer-widget">
-					<aside class="widget">
-						<h2 class="widget-title">Our Company</h2>
-						<ul class="menu">
-							<li><a href="#">About Us</a></li>
-							<li><a href="#">Services</a></li>
-						</ul>
-					</aside>
-				</div>
-				<div class="col-md-6 col-lg-3 pbmit-footer-widget">
-					<aside class="widget">
-						<h2 class="widget-title">Our Servcices</h2>
-						<ul class="menu">
-							<li><a href="#">Machine Analysis</a></li>
-							<li><a href="#">Comprehensive Logits</a></li>
-							<li><a href="#">Plant Maintenance</a></li>
-							<li><a href="#">Maintenance & Repairing</a></li>
-						</ul>
-					</aside>
-				</div>
-				<div class="col-md-6 col-lg-3 pbmit-footer-widget">
-					<aside class="widget">
-						<h2 class="widget-title">Media</h2>
-						<ul class="menu">
-							<li><a href="#">News</a></li>
-							<li><a href="#">Events</a></li>
-							<li><a href="#">Image Gallery</a></li>
-						</ul>
-					</aside>
-				</div>
-				<div class="col-md-6 col-lg-3 pbmit-footer-widget">
-					<aside class="widget">
-						<h2 class="widget-title">Quick Link</h2>
-						<ul class="menu">
-							<li><a href="faq.html">Privacy Policy</a></li>
-							<li><a href="faq.html">Terms of use</a></li>
-						</ul>
-					</aside>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="pbmit-footer-text-area">
-		<div class="container">
-			<div class="pbmit-footer-text-inner">
-				<div class="row">
-					<div class="col-md-6">
-						<div class="pbmit-footer-copyright-text-area">
-							<div class="copyright-text">
-								<a href=".">Veritas Afrika</a><span class="fw-bold"> © 2025</span>. All Right Reserved
-								Developed By <a href="https://tetercreatives.com" target="_blank">Teter PLC</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class=" pbmit-footer-social-area">
-							<ul class="pbmit-social-links">
-								<li class="pbmit-social-li pbmit-social-facebook">
-									<a title="Facebook" href="#" target="_blank">
-										<span><i class="pbmit-base-icon-facebook-f"></i></span>
-									</a>
-								</li>
-								<li class="pbmit-social-li pbmit-social-twitter">
-									<a title="Twitter" href="#" target="_blank">
-										<span><i class="pbmit-base-icon-twitter-2"></i></span>
-									</a>
-								</li>
-								<li class="pbmit-social-li pbmit-social-youtube">
-									<a title="Youtube" href="#" target="_blank">
-										<span><i class="pbmit-base-icon-youtube-play"></i></span>
-									</a>
-								</li>
-								<li class="pbmit-social-li pbmit-social-instagram">
-									<a title="Instagram" href="#" target="_blank">
-										<span><i class="pbmit-base-icon-instagram"></i></span>
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="pbmit-footer-big-area-wrapper">
+        <div class="pbmit-footer-big-area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-12 col-lg-6 pbmit-footer-left">
+                        <h3>Delivering excellence, one project at a time.</h3>
+                    </div>
+                    <div class="col-md-12 col-lg-6 text-lg-end">
+                        <div class="pbmit-footer-logo">
+                            <img class="pbmit-main-logo" src="{{ asset('assets/images/logo/logo-05.png') }}"
+                                alt="Logo">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pbmit-footer-contact-area-wrapper">
+        <div class="pbmit-footer-contact-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-lg-4 pbmit-footer-contact-box">
+                        <span>Hia cinema, JubaSouth Sudan</span>
+                    </div>
+                    <div class="col-md-12 col-lg-4 pbmit-footer-contact-box">
+                        <span><a href="https://induyst-demo.pbminfotech.com/cdn-cgi/l/email-protection"
+                                class="__cf_email__"
+                                data-cfemail="d6b5b9b8a2b7b5a296b3aeb7bba6bab3f8b5b9bb">[email&#160;protected]</a></span>
+                    </div>
+                    <div class="col-md-12 col-lg-4 pbmit-footer-contact-box">
+                        <span>+211 923 2 41 605</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pbmit-footer-widget-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-3 pbmit-footer-widget">
+                    <aside class="widget">
+                        <h2 class="widget-title">Our Company</h2>
+                        <ul class="menu">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Services</a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-md-6 col-lg-3 pbmit-footer-widget">
+                    <aside class="widget">
+                        <h2 class="widget-title">Our Servcices</h2>
+                        <ul class="menu">
+                            <li><a href="#">Machine Analysis</a></li>
+                            <li><a href="#">Comprehensive Logits</a></li>
+                            <li><a href="#">Plant Maintenance</a></li>
+                            <li><a href="#">Maintenance & Repairing</a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-md-6 col-lg-3 pbmit-footer-widget">
+                    <aside class="widget">
+                        <h2 class="widget-title">Media</h2>
+                        <ul class="menu">
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Image Gallery</a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-md-6 col-lg-3 pbmit-footer-widget">
+                    <aside class="widget">
+                        <h2 class="widget-title">Quick Link</h2>
+                        <ul class="menu">
+                            <li><a href="faq.html">Privacy Policy</a></li>
+                            <li><a href="faq.html">Terms of use</a></li>
+                        </ul>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pbmit-footer-text-area">
+        <div class="container">
+            <div class="pbmit-footer-text-inner">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="pbmit-footer-copyright-text-area">
+                            <div class="copyright-text">
+                                <a href=".">Veritas Afrika</a><span class="fw-bold"> © 2025</span>. All Right
+                                Reserved
+                                Developed By <a href="https://tetercreatives.com" target="_blank">Teter PLC</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class=" pbmit-footer-social-area">
+                            <ul class="pbmit-social-links">
+                                <li class="pbmit-social-li pbmit-social-facebook">
+                                    <a title="Facebook" href="#" target="_blank">
+                                        <span><i class="pbmit-base-icon-facebook-f"></i></span>
+                                    </a>
+                                </li>
+                                <li class="pbmit-social-li pbmit-social-twitter">
+                                    <a title="Twitter" href="#" target="_blank">
+                                        <span><i class="pbmit-base-icon-twitter-2"></i></span>
+                                    </a>
+                                </li>
+                                <li class="pbmit-social-li pbmit-social-youtube">
+                                    <a title="Youtube" href="#" target="_blank">
+                                        <span><i class="pbmit-base-icon-youtube-play"></i></span>
+                                    </a>
+                                </li>
+                                <li class="pbmit-social-li pbmit-social-instagram">
+                                    <a title="Instagram" href="#" target="_blank">
+                                        <span><i class="pbmit-base-icon-instagram"></i></span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 <!-- footer End -->
