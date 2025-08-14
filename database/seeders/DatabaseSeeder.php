1cdc7993693dbe11317001f2b59142f5a44098bc
abela12 <abelberhanu6@gmail.com>
2025-08-14 12:16:29 +0300

    feat: include ServiceSeeder in DatabaseSeeder for service data population

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             PostCategorySeeder::class,
             PostSeeder::class,
             PageSeeder::class,
+            ServiceSeeder::class,
         ]);
     }
 }
