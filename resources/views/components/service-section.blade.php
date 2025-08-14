3be8ae5e252f6e20d15c75ffb5b7541226f132cf
abela12 <abelberhanu6@gmail.com>
2025-08-14 12:16:58 +0300

    refactor: add conditional rendering for services in ServiceSection component

--- resources/views/components/service-section.blade.php
+++ resources/views/components/service-section.blade.php
@@ -8,52 +8,62 @@
             <div class="service-arrow d-inline-flex"></div>
         </div>
 
-        <div class="swiper-slider" data-autoplay="true" data-loop="true" data-dots="false" data-arrows="true"
-            data-arrows-class="service-arrow" data-columns="3" data-margin="30" data-effect="slide">
-            <div class="swiper-wrapper">
-                @foreach ($services as $service)
-                    <!-- Slide {{ $loop->iteration }} -->
-                    <article class="pbmit-service-style-1 swiper-slide">
-                        <div class="pbminfotech-post-item">
-                            <div class="pbmit-box-content-wrap">
-                                <div class="pbmit-image-wrap">
-                                    <div class="pbmit-featured-img-wrapper">
-                                        <div class="pbmit-featured-wrapper">
-                                            <img src="{{ $service->main_image_url }}" class="img-fluid"
-                                                alt="{{ $service->title }}">
+        @if(isset($services) && $services && $services->count() > 0)
+            <div class="swiper-slider" data-autoplay="true" data-loop="true" data-dots="false" data-arrows="true"
+                data-arrows-class="service-arrow" data-columns="3" data-margin="30" data-effect="slide">
+                <div class="swiper-wrapper">
+                    @foreach ($services as $service)
+                        <!-- Slide {{ $loop->iteration }} -->
+                        <article class="pbmit-service-style-1 swiper-slide">
+                            <div class="pbminfotech-post-item">
+                                <div class="pbmit-box-content-wrap">
+                                    <div class="pbmit-image-wrap">
+                                        <div class="pbmit-featured-img-wrapper">
+                                            <div class="pbmit-featured-wrapper">
+                                                <img src="{{ $service->main_image_url }}" class="img-fluid"
+                                                    alt="{{ $service->title }}">
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="pbmit-service-icon-wrapper">
-                                        <div class="pbmit-service-icon">
-                                            {!! $service->svg_inline !!}
+                                        <div class="pbmit-service-icon-wrapper">
+                                            <div class="pbmit-service-icon">
+                                                {!! $service->svg_inline !!}
+                                            </div>
                                         </div>
-                                    </div>
 
-                                </div>
-                                <div class="pbmit-service-content-wrap">
-                                    <h3 class="pbmit-service-title">
-                                        {{ $service->title }}
-                                    </h3>
-                                    <div class="pbmit-service-description">
-                                        <p>{{ $service->short_description }}</p>
                                     </div>
-                                    <div class="pbmit-service-btn-wrapper">
-                                        <div class="pbmit-service-btn">
-                                            <a class="pbmit-button-inner"
-                                                href="{{ route('service.show', $service->slug) }}">
-                                                <span class="pbmit-button-text">Read More</span>
-                                                <i class="pbmit-base-icon-right-arrow"></i>
-                                            </a>
+                                    <div class="pbmit-service-content-wrap">
+                                        <h3 class="pbmit-service-title">
+                                            {{ $service->title }}
+                                        </h3>
+                                        <div class="pbmit-service-description">
+                                            <p>{{ $service->short_description }}</p>
                                         </div>
-                                    </div>
+                                        <div class="pbmit-service-btn-wrapper">
+                                            <div class="pbmit-service-btn">
+                                                <a class="pbmit-button-inner"
+                                                    href="{{ route('service.show', $service->slug) }}">
+                                                    <span class="pbmit-button-text">Read More</span>
+                                                    <i class="pbmit-base-icon-right-arrow"></i>
+                                                </a>
+                                            </div>
+                                        </div>
 
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </article>
-                @endforeach
+                        </article>
+                    @endforeach
+                </div>
             </div>
-        </div>
+        @else
+            <div class="text-center py-5">
+                <div class="pbmit-heading-subheading">
+                    <h4 class="pbmit-subtitle">Coming Soon</h4>
+                    <h3 class="pbmit-title">Our Services</h3>
+                </div>
+                <p class="lead">We're currently preparing our service offerings. Please check back soon!</p>
+            </div>
+        @endif
 
         <div class="pbmit-bottom-text text-center mt-5 pt-md-2">
             Latest solutions, and decades of experience. <a href="#"><u>Explore All Services</u></a>
