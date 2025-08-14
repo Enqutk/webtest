a62b78e9b94bd653c3fdf13af655cc09289a93f8
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-14 11:39:32 +0300

    fix: sanitize service description and features in service detail view

--- resources/views/services/service-detail.blade.php
+++ resources/views/services/service-detail.blade.php
@@ -1,34 +1,35 @@
 @extends('layouts.inner')
 
 @section('content')
-<section class="section-xxl fade-section">
-    <div class="container">
-        <div class="pbmit-heading-subheading mb-4">
-            <h4 class="pbmit-subtitle">Service Detail</h4>
-            <h2 class="pbmit-title">{{ $service->title }}</h2>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 mb-4">
-                <img src="{{ $service->secondary_image_url }}" class="img-fluid" alt="{{ $service->title }}">
+    <section class="section-xxl fade-section">
+        <div class="container">
+            <div class="pbmit-heading-subheading mb-4">
+                <h4 class="pbmit-subtitle">Service Detail</h4>
+                <h2 class="pbmit-title">{{ $service->title }}</h2>
             </div>
-            <div class="col-lg-6">
-                <div class="pbmit-service-description">
-                    <h5>Description</h5>
-                    <p>{!! $service->description !!}</p>
+            <div class="row">
+                <div class="col-lg-6 mb-4">
+                    <img src="{{ $service->secondary_image_url }}" class="img-fluid" alt="{{ $service->title }}">
                 </div>
-                @if ($service->features)
-                    <div class="pbmit-service-features mt-3">
-                        <h5>Features</h5>
-                        <div>{!! $service->features !!}</div>
+                <div class="col-lg-6">
+                    <div class="pbmit-service-description">
+                        <h5>Description</h5>
+                        <p>{!! \Purifier::clean($service->description) !!}</p>
                     </div>
-                @endif
-                @if ($service->quote)
-                    <blockquote class="blockquote mt-3">
-                        <p class="mb-0">{{ $service->quote }}</p>
-                    </blockquote>
-                @endif
+                    @if ($service->features)
+                        <div class="pbmit-service-features mt-3">
+                            <h5>Features</h5>
+                            <div>{!! \Purifier::clean($service->features) !!}</div>
+                        </div>
+                    @endif
+                    @if ($service->quote)
+                        <blockquote class="blockquote mt-3">
+                            <p class="mb-0">{{ $service->quote }}</p>
+                        </blockquote>
+                    @endif
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
+
