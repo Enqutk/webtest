d29052bc0b51655e8e04a565f9344470f939ad89
fitsumhelina <dev.fitsum@gmail.com>
2025-08-15 16:12:46 +0300

    fix: correct background image URL syntax in About section component

--- resources/views/components/about-section.blade.php
+++ resources/views/components/about-section.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 col-xl-5 about-two-left-col">
-                <div class="about-two-img" style="background-image: url({{ $image }})"></div>  </div>
+                <div class="about-two-img" style="background-image: url('{{ $image }}');"></div>  </div>
             <div class="col-md-12 col-xl-7 about-two-right-col">
                 <div class="about-two-content">
                     <div class="pbmit-heading-subheading">
