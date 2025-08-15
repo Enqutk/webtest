e8138c2026bac7de328e6943767b086c616c30b3
fitsumhelina <dev.fitsum@gmail.com>
2025-08-15 10:35:37 +0300

    fix: update default location value in menu_locations migration to use MenuLocationEnum

--- database/migrations/2025_08_09_133521_create_menu_locations_table.php
+++ database/migrations/2025_08_09_133521_create_menu_locations_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Enums\LocationEnum;
+use App\Enums\MenuLocationEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 120);
             $table->string('slug', 120)->unique();
-            $table->string('location')->default( LocationEnum::Navbar->value);
+            $table->string('location')->default( MenuLocationEnum::Navbar->value);
             $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
