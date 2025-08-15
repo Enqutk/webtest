2ba3e9951b4e30fd7d57820164e2f361f9fbe3b6
fitsumhelina <dev.fitsum@gmail.com>
2025-08-15 14:25:29 +0300

    feat: refactor hero features section to use data binding and improve structure

--- resources/views/components/hero-features.blade.php
+++ resources/views/components/hero-features.blade.php
@@ -1,31 +1,26 @@
-@props(['features' => []])
-
-<section class="section-sm border-bottom ihbox-section-two">
-    <div class="container">
-        <div class="row">
-            @php
-                $items = [];
-                if (!empty($features) && isset($features[0])) {
-                    $items = $features[0]->list_items ?? [];
-                }
-            @endphp
-            @foreach($items as $feature)
-            <div class="col-md-4 pbmit-column">
-                <div class="pbmit-ihbox-style-4">
-                    <div class="pbmit-ihbox-box">
-                        <div class="pbmit-ihbox-icon">
-                            <div class="pbmit-ihbox-icon-wrapper pbmit-icon-type-icon">
-                                {!! $feature['icon'] ?? '' !!}
-                            </div>
+<div class="container">
+    <div class="row">
+        @foreach($data['heroFeatures'] as $feature)
+        @foreach($feature->list_items as $item)
+        <div class="col-md-4 pbmit-column">
+            <div class="pbmit-ihbox-style-4">
+                <div class="pbmit-ihbox-box">
+                    <div class="pbmit-ihbox-icon">
+                        <div class="pbmit-ihbox-icon-wrapper pbmit-icon-type-icon">
+                            {{-- Optionally display an icon if available --}}
+                            @if(!empty($item['icon']))
+                            <i class="{{ $item['icon'] }}"></i>
+                            @endif
                         </div>
-                        <div class="pbmit-ihbox-contents">
-                            <h2 class="pbmit-element-title">{{ $feature['title'] ?? '' }}</h2>
-                            <div class="pbmit-heading-desc">{{ $feature['description'] ?? '' }}</div>
-                        </div>
+                    </div>
+                    <div class="pbmit-ihbox-contents">
+                        <h2 class="pbmit-element-title">{{ $item['title'] ?? '' }}</h2>
+                        <div class="pbmit-heading-desc">{{ $item['description'] ?? '' }}</div>
                     </div>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
+        @endforeach
     </div>
-</section>
+</div>
