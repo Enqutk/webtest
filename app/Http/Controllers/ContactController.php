86a02b3a3b8b96c4add31e56f36003d1e74cf66b
fitsumhelina <dev.fitsum@gmail.com>
2025-08-16 08:43:27 +0300

    fix: update success message in send method of ContactController for clarity

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -41,7 +41,7 @@ class ContactController extends Controller
             });
 
 
-        return back()->with('success', 'Your message has been sent!');
+        return back()->with('success', 'Your message has been sent! We will get back to you shortly.');
     }
 
 }
