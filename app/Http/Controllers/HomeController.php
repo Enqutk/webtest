73ec8bdba7558b0dcf8f41b37cbf2541d4613205
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-16 09:45:34 +0300

    refactor: enhance code formatting and readability in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\Service;
 use App\Enums\StatusEnum;
+use App\Models\Post;
 
-class HomeController extends Controller {
-    public function index() {
-        $services = Service::where( 'status', StatusEnum::active )
-        ->orderBy( 'order' )
-        ->take( 6 )
-        ->get();
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $services = Service::where('status', StatusEnum::active)
+            ->orderBy('order')
+            ->take(6)
+            ->get();
 
-        return view( 'index', compact( 'services' ) );
+        return view('index', compact('services'));
+    }
+
+    public function PostIndex()
+    {
+        $blogPosts = Post::with(['category', 'creator'])
+            ->where('is_active', true)
+            ->latest()
+            ->take(6)
+            ->get();
+
+        return view('blog.index', compact('blogPosts'));
+    }
+
+    public function PostShow($slug)
+    {
+        $post = Post::with(['category', 'creator'])
+            ->where('slug', $slug)
+            ->where('is_active', true)
+            ->firstOrFail();
+
+        $gallery = $post->getMedia('gallery');
+
+        return view('blog.show', compact('post', 'gallery'));
     }
 }
