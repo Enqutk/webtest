e0f3d0f06688efc16601c8c0be3c764062157855
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-16 17:52:51 +0300

    refactor: replace hardcoded social media links with SocialMedia component in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -96,28 +96,7 @@
                     </div>
                     <div class="col-md-6">
                         <div class=" pbmit-footer-social-area">
-                            <ul class="pbmit-social-links">
-                                <li class="pbmit-social-li pbmit-social-facebook">
-                                    <a title="Facebook" href="#" target="_blank">
-                                        <span><i class="pbmit-base-icon-facebook-f"></i></span>
-                                    </a>
-                                </li>
-                                <li class="pbmit-social-li pbmit-social-twitter">
-                                    <a title="Twitter" href="#" target="_blank">
-                                        <span><i class="pbmit-base-icon-twitter-2"></i></span>
-                                    </a>
-                                </li>
-                                <li class="pbmit-social-li pbmit-social-youtube">
-                                    <a title="Youtube" href="#" target="_blank">
-                                        <span><i class="pbmit-base-icon-youtube-play"></i></span>
-                                    </a>
-                                </li>
-                                <li class="pbmit-social-li pbmit-social-instagram">
-                                    <a title="Instagram" href="#" target="_blank">
-                                        <span><i class="pbmit-base-icon-instagram"></i></span>
-                                    </a>
-                                </li>
-                            </ul>
+                             <x-social-media />
                         </div>
                     </div>
                 </div>
