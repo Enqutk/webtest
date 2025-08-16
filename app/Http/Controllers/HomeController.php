b7a0ec70466f27a79c3891910ced3f1ac1a77000
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-16 15:31:20 +0300

    fix: enhance postShow and postsByCategory methods to include recent posts and categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Service;
 use App\Enums\StatusEnum;
 use App\Models\Post;
+use App\Models\PostCategory;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -35,9 +37,46 @@ class HomeController extends Controller
             ->where('slug', $slug)
             ->where('is_active', true)
             ->firstOrFail();
-
+  $recentPosts = Post::where('is_active', true)
+        ->where('id', '!=', $post->id)
+        ->latest()
+        ->take(5)
+        ->get();
+        $categories = PostCategory::all();
         $gallery = $post->getMedia('gallery');
 
-        return view('blog.show', compact('post', 'gallery'));
+        return view('blog.show', compact('post', 'gallery', 'categories', 'recentPosts'));
     }
+public function postsByCategory($slug)
+{
+    // Get category by slug
+    $category = PostCategory::where('slug', $slug)->firstOrFail();
+
+    // Get posts for this category
+    $posts = Post::where('category_id', $category->id)
+                 ->where('is_active', 1)
+                 ->latest()
+                 ->get();
+
+    // Get all categories for sidebar
+    $categories = PostCategory::withCount('posts')->get();
+
+    // Get recent posts
+    $recentPosts = Post::where('is_active', 1)->latest()->take(5)->get();
+
+    return view('blog.category', compact('category', 'posts', 'categories', 'recentPosts'));
+}
+
+    public function search(Request $request)
+{
+    $query = $request->input('q');
+
+    $posts = Post::where('title', 'like', "%{$query}%")
+                 ->orWhere('content', 'like', "%{$query}%")
+                 ->latest()
+                 ->paginate(10);
+
+    return view('blog.search', compact('posts', 'query'));
+}
+
 }
