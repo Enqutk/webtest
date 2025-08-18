373204eee796d5cea8d941113ac478d3322d403c
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-18 09:59:22 +0300

    refactor: update routing to use BlogController and ServiceController

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BlogController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/blog', [HomeController::class, 'postIndex'])->name('blog.index');
-Route::get('/blog/{slug}', [HomeController::class, 'postShow'])->name('blog.show');
-Route::get('/blog/category/{slug}', [HomeController::class, 'postsByCategory'])->name('blog.category');
+Route::get('/', [ServiceController::class, 'index'])->name('home');
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
+Route::get('/blog/category/{slug}', [BlogController::class, 'postsByCategory'])->name('blog.category');
 
 
 Route::get('/about', function () {
@@ -18,5 +19,5 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/our-services', [ServiceController::class, 'index'])->name('services.index');
+Route::get('/our-services', [ServiceController::class, 'servicesIndex'])->name('services.index');
 Route::get('/services/{slug}', [ServiceController::class, 'show'])->name('services.show');
