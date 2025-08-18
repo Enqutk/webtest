ea0591ac5fe3cbf6517b68494bef30cd2df90ad7
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-18 09:30:49 +0300

    refactor: streamline ServiceSection component constructor and formatting

--- app/View/Components/ServiceSection.php
+++ app/View/Components/ServiceSection.php
@@ -8,28 +8,26 @@ use Illuminate\View\Component;
 use App\Models\Service;
 use App\Enums\StatusEnum;
 
-class ServiceSection extends Component
-{
+class ServiceSection extends Component {
     public $services;
 
     /**
-     * Create a new component instance.
-     */
-    public function __construct($services = null)
-    {
+    * Create a new component instance.
+    */
+
+    public function __construct( $services = null ) {
         $this->services = $services;
-        if (is_null($this->services)) {
-            $this->services = Service::where('status', StatusEnum::active)->orderBy('order')->take(5)->get();
+        if ( is_null( $this->services ) ) {
+            $this->services = Service::activeOrdered( 5 )->get();
         }
     }
 
     /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
-    {
-        return view('components.service-section');
+    * Get the view / contents that represent the component.
+    */
+
+    public function render(): View|Closure|string {
+        return view( 'components.service-section' );
     }
 }
-
 
