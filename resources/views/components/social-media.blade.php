0d3f1c1432770319c620af5fc107de50483eec0e
Yabe-B <yeabsirabehailu92@gmail.com>
2025-08-18 06:11:24 +0300

    refactor: update social media links to use dynamic data from socialRefs

--- resources/views/components/social-media.blade.php
+++ resources/views/components/social-media.blade.php
@@ -1,27 +1,13 @@
-
-                            <div class="pbmit-header-social">
-                                <ul class="pbmit-social-links">
-                                    <li class="pbmit-social-li pbmit-social-facebook">
-                                        <a title="Facebook" href="#" target="_blank">
-                                            <span><i class="pbmit-base-icon-facebook-f"></i></span>
-                                        </a>
-                                    </li>
-                                    <li class="pbmit-social-li pbmit-social-twitter">
-                                        <a title="Twitter" href="#" target="_blank">
-                                            <span><i class="pbmit-base-icon-twitter-2"></i></span>
-                                        </a>
-                                    </li>
-                                    <li class="pbmit-social-li pbmit-social-youtube">
-                                        <a title="Youtube" href="#" target="_blank">
-                                            <span><i class="pbmit-base-icon-youtube-play"></i></span>
-                                        </a>
-                                    </li>
-                                    <li class="pbmit-social-li pbmit-social-instagram">
-                                        <a title="Instagram" href="#" target="_blank">
-                                            <span><i class="pbmit-base-icon-instagram"></i></span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                           
-                        
+<div class="pbmit-header-social">
+    <ul class="pbmit-social-links">
+        @foreach ($socialRefs as $ref)
+            <li class="pbmit-social-li" title="{{ $ref->title }}">
+                <a href="{{ $ref->link }}" target="_blank">
+                    <span>
+                        <i class="{{ $ref->icon_class }}"></i>
+                    </span>
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
