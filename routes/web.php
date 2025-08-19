eb56754e77530801437cca253b3d6be0e9c5a3e4
fitsumhelina <dev.fitsum@gmail.com>
2025-08-19 15:51:18 +0300

    refactor: remove BlogController routes for cleanup

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\BlogController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -18,10 +17,6 @@ Route::get('/services/{slug}', [ServiceController::class, 'show'])->name('servic
 
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact/send/{recipient}', [ContactController::class, 'send'])->name('contact.send');
-
-Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
-Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
-Route::get('/blog/category/{slug}', [BlogController::class, 'postsByCategory'])->name('blog.category');
 
 
 
