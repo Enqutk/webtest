80b6a50014dc8ad6521746cd26700524ebac5759
fitsumhelina <dev.fitsum@gmail.com>
2025-08-19 17:39:10 +0300

    refactor: simplify about section layout and remove unused title and subtitle

--- resources/views/components/about-section.blade.php
+++ resources/views/components/about-section.blade.php
@@ -1,24 +1,13 @@
-
-
 <section class="section-lg">
     <div class="container">
-        <div class="row">
+        <div class="row mb-5">
             <div class="col-md-12 col-xl-5 about-two-left-col">
                 <div class="about-two-img" style="background-image: url('{{ $image }}');"></div>  
             </div>
             <div class="col-md-12 col-xl-7 about-two-right-col">
                 <div class="about-two-content">
                     <div class="pbmit-heading-subheading">
-                        <h4 class="pbmit-subtitle">{{ $subtitle }}</h4>
-                        <h2 class="pbmit-title">{{ $title }}</h2>
-                    </div>
-                    <div class="inner-box">
-                        <div class="row">
-                            <div class="col-md-10">
-                                <p>{{ $description }}</p>
-                            </div>
-                          
-                        </div>
+                        <h5 class="">{{ $description }}</h5>
                     </div>
                 </div>
             </div>
