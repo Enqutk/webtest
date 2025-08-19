1b59ac5ccded606b66d30e610e5254af1a41d41c
fitsumhelina <dev.fitsum@gmail.com>
2025-08-19 17:12:25 +0300

    refactor: update about feature component to improve layout and heading styles

--- resources/views/components/about-feature.blade.php
+++ resources/views/components/about-feature.blade.php
@@ -1,25 +1,23 @@
-
-
 <section class="section-lg">
     <div class="container">
-        <div class="row mb-4">
+        <div class="row mb-5">
             <div class="col-md-12 col-xl-5 about-two-left-col">
                 <div class="about-two-img" style="background-image: url('{{ $image1 }}');"></div>  
             </div>
             <div class="col-md-12 col-xl-7 about-two-right-col">
                 <div class="about-two-content">
                     <div class="pbmit-heading-subheading">
-                        <h2 class="">{{ $description1 }}</h2>
+                        <h5 class="">{{ $description1 }}</h5>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="row mt-4">
+        <div class="row mb-5 mt-4">
             <div class="col-md-12 col-xl-7 about-two-right-col">
                 <div class="about-two-content">
                     <div class="pbmit-heading-subheading">
-                        <h2 class="">{{ $description2 }}</h2>
+                        <h5 class="">{{ $description2 }}</h5>
                     </div>
                 </div>
             </div>
