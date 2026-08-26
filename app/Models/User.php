24420be1c06cd8a3a40a40c6e376fa6c1f8c6084
enqutk <enkukokob@gmail.com>
2026-08-26 12:07:50 +0300

    feat: implement FilamentUser interface and access control in User model
    - Added FilamentUser interface to the User model for integration with Filament.
    - Implemented canAccessPanel method to control access to the panel based on user roles, enhancing security and role management.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,37 +2,43 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
-
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
-  
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-   
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-  
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() !== 'mgt') {
+            return false;
+        }
+
+        return $this->hasAnyRole(['admin', 'moderator', 'blogger']);
     }
 }
