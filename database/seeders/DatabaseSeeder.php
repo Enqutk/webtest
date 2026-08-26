ff5fa80b39912bcf8b0f64617f71acdfd44bb828
enqutk <enkukokob@gmail.com>
2026-08-26 10:48:59 +0300

    feat: include AboutContentSeeder in DatabaseSeeder for content initialization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             TeamSeeder::class,
             EntitySeeder::class,
             StatsSeeder::class,
+            AboutContentSeeder::class,
         ]);
     }
 }
